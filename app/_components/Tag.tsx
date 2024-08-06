4bd009ffc2f3fe4ce3a39c925bb016354ada303a
Zaklina Radenkovic <zradenkovic@hotmail.com>
2024-08-06 10:33:32 +0200

    building Tag component

---
+++ app/_components/Tag.tsx
@@ -0,0 +1,18 @@
+function Tag({
+  color,
+  background,
+  children,
+}: {
+  color: String;
+  background: String;
+  children: String;
+}) {
+  return (
+    <span
+      className={`max-w-fit rounded-full px-3 py-1 text-sm font-semibold uppercase ${color} ${background}`}
+    >
+      {children}
+    </span>
+  );
+}
+export default Tag;
