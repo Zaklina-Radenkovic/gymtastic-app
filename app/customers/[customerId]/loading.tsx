72a45dd5c39824226bbbb15a2a9739a883341b39
Zaklina Radenkovic <zradenkovic@hotmail.com>
2024-08-14 09:17:11 +0200

    setting loading indicator for fetching customers and single customer

--- app/customers/[customerId]/loading.tsx
+++ app/customers/[customerId]/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="grid items-center justify-center">
       <Spinner />
-      <p className="text-xl">Loading customers...</p>
+      <p className="text-xl">Loading customer...</p>
     </div>
   );
 }
